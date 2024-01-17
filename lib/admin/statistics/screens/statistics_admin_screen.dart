1a113237a42b4a96ae2ea1ade6e70e8453233a43
VihaoHT <haoanna1232@gmail.com>
2024-01-18 03:09:44 +0700

    UI Chart complete

--- lib/admin/statistics/screens/statistics_admin_screen.dart
+++ lib/admin/statistics/screens/statistics_admin_screen.dart
@@ -1,10 +1,5 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_booking_app/core/components/header_admin.dart';
-
-import '../../../auth/bloc/auth_bloc.dart';
-import '../../../core/constants/constants.dart';
-
 class StatisticsAdminScreen extends StatefulWidget {
   const StatisticsAdminScreen({super.key});
 
@@ -17,14 +12,177 @@ class _StatisticsAdminScreenState extends State<StatisticsAdminScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
+          body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16,top:50,right: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              HeaderAdmin(title: "Statistics screen"),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ColorSuggest(
+                    color: Colors.blue,
+                    title: "January",
+                  ),
+                  ColorSuggest(
+                    color: Colors.red,
+                    title: "February",
+                  ),
+                  ColorSuggest(
+                    color: Colors.yellow,
+                    title: "March",
+                  ),
+                  ColorSuggest(
+                    color: Colors.green,
+                    title: "April",
+                  ),
+                  ColorSuggest(
+                    color: Colors.cyanAccent,
+                    title: "May",
+                  ),
+                  ColorSuggest(
+                    color: Colors.pinkAccent,
+                    title: "June",
+                  ),
+                  ColorSuggest(
+                    color: Colors.purple,
+                    title: "July",
+                  ),
+                  ColorSuggest(
+                    color: Colors.teal,
+                    title: "August",
+                  ),
+                  ColorSuggest(
+                    color: Colors.grey,
+                    title: "September",
+                  ),
+                  ColorSuggest(
+                    color: Colors.blueGrey,
+                    title: "October",
+                  ),
+                  ColorSuggest(
+                    color: Colors.amber,
+                    title: "November",
+                  ),
+                  ColorSuggest(
+                    color: Colors.orange,
+                    title: "December",
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  const Text(
+                    "Monthly Earnings All Sold Tickets",
+                    style: TextStyle(
+                        color: Colors.white54,
+                        fontSize: 34,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 700,
+                    height: 700,
+                    child: PieChart(
+                        swapAnimationCurve: Curves.easeInOutQuint,
+                        swapAnimationDuration:
+                            const Duration(milliseconds: 750),
+                        PieChartData(sections: [
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.blue,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.red,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.yellow,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.green,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.cyanAccent,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.pinkAccent,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.purple,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.teal,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.grey,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.blueGrey,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.amber,
+                          ),
+                          PieChartSectionData(
+                            value: 20,
+                            color: Colors.orange,
+                          ),
+                        ])),
+                  )
+                ],
+              ),
             ],
           ),
         ),
+      )),
+    );
+  }
+}
+
+class ColorSuggest extends StatelessWidget {
+  final Color color;
+  final String title;
+
+  const ColorSuggest({
+    super.key,
+    required this.color,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Container(
+            width: 30,
+            height: 30,
+            decoration: BoxDecoration(
+              color: color,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 10),
+            child: Text(
+              title,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500),
+            ),
+          ),
+        ],
       ),
     );
   }
